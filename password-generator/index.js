c2f222b6602dbd8fff8dffdd0ab82be69b252ff0
Hunter Bell <bell.hmj@icloud.com>
2026-01-29 05:16:58 -0500

    Retry after review

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,40 +1,50 @@
-
 let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordLength = 15;
-let passwordsGenerated = false;
+const passwordLength = 15;
+
+let password1 = document.querySelector("#password1");
+let password2 = document.querySelector("#password2");
 
 
-let password1 = document.querySelector('#password1');
-let password2 = document.querySelector('#password2');
 
 
-function generatePassword() {
-   if (password1.textContent === "" && password2.textContent === "") { 
-    for (let i = 0; i < passwordLength; i++) {
-        let index1 = Math.floor(Math.random() * characters.length);
-        let index2 = Math.floor(Math.random() * characters.length);
-        password1.textContent += characters[index1];
-        password2.textContent += characters[index2];
-    }
-
-    } else if (password1.textContent != "" && password2.textContent != "") {
-        password1.textContent = "";
-        password2.textContent = "";
-        for (let i = 0; i < passwordLength; i++) {
-        let index1 = Math.floor(Math.random() * characters.length);
-        let index2 = Math.floor(Math.random() * characters.length);
-        password1.textContent += characters[index1];
-        password2.textContent += characters[index2];
-    }
-    
-    
-    }}
 
 
-    //     if (password1.textContent != "") {
-    //     password1.textContent = ""
-    // }
-    //     console.log("no")
+
+
+
+
+
+
+
+
+
+
+
+
+// First attempt no help:
+
+// let password1 = document.querySelector('#password1');
+// let password2 = document.querySelector('#password2');
+
+
+// function generatePassword() {
+//    if (password1.textContent === "" && password2.textContent === "") { 
+//     for (let i = 0; i < passwordLength; i++) {
+//         let index1 = Math.floor(Math.random() * characters.length);
+//         let index2 = Math.floor(Math.random() * characters.length);
+//         password1.textContent += characters[index1];
+//         password2.textContent += characters[index2];
+//     }
+
+//     } else if (password1.textContent != "" && password2.textContent != "") {
+//         password1.textContent = "";
+//         password2.textContent = "";
+//         for (let i = 0; i < passwordLength; i++) {
+//         let index1 = Math.floor(Math.random() * characters.length);
+//         let index2 = Math.floor(Math.random() * characters.length);
+//         password1.textContent += characters[index1];
+//         password2.textContent += characters[index2];
+//     }}}
     
